791a2e91f2b0b703a9fb62b10383c930d3bec53a
nobuyoshi <nobuyoshi_japa@hotmail.com>
2021-07-06 20:02:53 -0300

    add validão do formulário

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -11,9 +13,49 @@ def logout(request):
 
 
 def cadastro(request):
-    print(request.POST)
-    return render(request, 'accounts/cadastro.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/cadastro.html')
 
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha2 = request.POST.get('senha2')
+
+
+    if not nome or not sobrenome or not email or not usuario or not senha or not senha2:
+        messages.error(request, 'Nenhum campo pode estar vazio.')
+        return render(request, 'accounts/cadastro.html')
+
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, 'Email Inválido.')
+        return render(request, 'accounts/cadastro.html')
+
+    if len(senha) <= 5:
+        messages.error(request, 'Senha precisa ter 6 caracteres ou mais.')
+        return render(request, 'accounts/cadastro.html')
+
+    if len(usuario) <= 5:
+        messages.error(request, 'Senha precisa ter 5 caracteres ou mais.')
+        return render(request, 'accounts/cadastro.html')
+
+    if senha != senha2:
+        messages.error(request, 'Senhas precisam ser iguais.')
+        return render(request, 'accounts/cadastro.html')
+
+    if User.objects.filter(username=usuario).exists():
+        messages.error(request, 'Usuário já existe.')
+        return render(request, 'accounts/cadastro.html')
+
+    if User.objects.filter(email=email).exists():
+        messages.error(request, 'Email já cadastrado.')
+        return render(request, 'accounts/cadastro.html')
+
+    messages.success(request, 'Registrado com sucesso!')
+    return render(request, 'accounts/cadastro.html')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
